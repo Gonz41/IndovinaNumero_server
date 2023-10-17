7bd6403227cb58d122ce140cf0543e19f935c705
lai2-24 <lai2-24@itismeucci.com>
2023-10-17 14:37:30 +0200

    consegna server

--- src/main/java/com/indovinanumero/App.java
+++ src/main/java/com/indovinanumero/App.java
@@ -1,5 +1,11 @@
 package com.indovinanumero;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            ServerSocket server = new ServerSocket(3000);
+            Socket s = server.accept();
+            System.out.println("Connessione effettuata.");
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            boolean indovina = true;
+            int number = (int)(Math.random()*100+1);
+            System.out.print("il numero e': " + number);
+            int risposta;
+            int cont = 0;
+            while(indovina){
+                cont++;
+                risposta = Integer.parseInt(in.readLine());
+                if(risposta == number ){
+                    out.writeBytes(3 + "\n");
+                    out.writeBytes(cont + "\n");
+                    indovina = false;
+                }else{
+                    if(risposta > number){
+                        out.writeBytes(2 + "\n");
+                    }else{
+                        out.writeBytes(1 + "\n");
+                    }
+                }
+            }
+            s.close();
+            System.out.println("Connessione chiusa.");
+            server.close();
+            
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'instanza.");
+            System.exit(1);
+        }
     }
 }
